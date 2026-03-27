534df710ea5c340ffea16ad1e7e286ab4f468d8f
Nenvinh <135128068+Nenvinh@users.noreply.github.com>
2026-03-27 21:48:49 +0100

    Add files via upload

--- Robot_projekt/main.py
+++ Robot_projekt/main.py
@@ -16,33 +16,35 @@ import math
 ev3 = EV3Brick()
 
 # motory
-left_motor = Motor(Port.B)
+left_motor = Motor(Port.D)
 right_motor = Motor(Port.C)
-radar_motor = Motor(Port.A)
+gate_motor = Motor(Port.A)
 
 # senzory
-ultra = UltrasonicSensor(Port.S1)
-color_sensor = ColorSensor(Port.S2)
+#ultra = UltrasonicSensor(Port.S1)
+#color_sensor = ColorSensor(Port.S2)
 gyro = GyroSensor(Port.S3)
+tlacitko = TouchSensor(Port.S2)
 
 
-robot = DriveBase(left_motor, right_motor, 56, 120)
+robot = DriveBase(left_motor, right_motor, 56, 145)
 gyro.reset_angle(0)
 
 
 # Write your program here.
 ev3.speaker.beep()
 
+
 def choose_color():
     colors = [
         (Color.RED, "CERVENA"),
         (Color.GREEN, "ZELENA"),
         (Color.BLUE, "MODRA"),
-        (Color.BLACK, "CERNA")
+        (Color.YELLOW, "ZLUTÁ")
     ]
     i = 0
     while True:
-        ev3.screen.clear()
+        #ev3.screen.clear()
         ev3.screen.print(colors[i][1])
         b = ev3.buttons.pressed()
         if Button.LEFT in b:
@@ -55,27 +57,42 @@ def choose_color():
             ev3.speaker.beep()
             return colors[i][0]
 
-TARGET_COLOR = choose_color()
 
-def radar():
+
 
 
 def turn_360_on_spot(speed=200):
-    # reset gyra
     gyro.reset_angle(0)
 
-    # rozjeti motoru (tank otaceni)
-    left_motor.run(-speed)
-    right_motor.run(speed)
+    left_motor.run(speed)
+    right_motor.run(-speed)
 
-    # cekani dokud nedosahneme 360°
+    next_stop = 24  # první zastavení na 24°
+    stop_counter = 0
     while abs(gyro.angle()) < 360:
+        angle = abs(gyro.angle())
+        ev3.screen.print(stop_counter)
+        ev3.screen.print(angle)
+        # pokud jsme dosáhli dalšího milníku
+        if angle >= next_stop:
+            left_motor.stop()
+            right_motor.stop()
+
+            wait(2000)  # pauza 2 sekundy
+            stop_counter = stop_counter + 1
+            # znovu rozjet
+            left_motor.run(speed)
+            right_motor.run(-speed)
+
+            next_stop += 20  # další zastávka (40, 60, ...)
+
         wait(10)
 
-    # zastaveni motoru
     left_motor.stop()
     right_motor.stop()
 
 # Hlavní program
-
+TARGET_COLOR = choose_color()
+ev3.screen.print("Hledaná barva: ")
+ev3.screen.print(TARGET_COLOR)
 turn_360_on_spot()
